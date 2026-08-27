eb9e6e31d777cd5d382d6a29dd7314f9dd29d84e
Sergio Wicramachine <wicramachine@gmail.com>
2026-08-27 15:08:25 +0200

    Revert "Merge pull request #3 from wicra/feat/Ayman"
    This reverts commit 7ab4ab35e5d32977a8f43b29f0e46b03fa5c18a9, reversing
    changes made to c1e05004ba315931aaa168ff20c3cd836f3a9c48.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
